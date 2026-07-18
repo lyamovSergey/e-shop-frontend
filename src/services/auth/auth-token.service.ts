fd7ee0f555724a96671d4cdffdf7b7ccbe766369
Sergey Lyamov <lyamofff@gmail.com>
2026-07-18 18:50:38 +0300

    fix auth

--- src/services/auth/auth-token.service.ts
+++ src/services/auth/auth-token.service.ts
@@ -12,7 +12,7 @@ export const getAccessToken = () => {
 
 export const saveTokenStorage = (accessToken: string) => {
 	Cookies.set(EnumTokens.ACCESS_TOKEN, accessToken, {
-		domain: process.env.APP_DOMAIN,
+		// domain: process.env.APP_DOMAIN,
 		sameSite: 'strict',
 		expires: 1
 	})
